6c69f17232c8ef549fe640c3a7f388e88680e2f8
Michael Thelander <mjthelander@gmail.com>
2014-09-23 21:01:55 -0700

    Exercises 5.8 & 5.9

--- chapter5/section_5_2.scm
+++ chapter5/section_5_2.scm
@@ -61,3 +61,66 @@
 
 ; Exercise 5.8 -------------------------------------------------------------------
 
+; Register a will contain the value 3.
+
+;; (define (extract-labels text receive)
+;;   (if (null? text)
+;;       (receive '() '())
+;;       (extract-labels (cdr text)
+;;        (lambda (insts labels)
+;;          (let ((next-inst (car text)))
+;;            (if (symbol? next-inst)
+;; 	       (if (assoc next-inst labels)
+;; 		   (error "Label is multiply defined --" next-inst)
+;; 		   (receive insts
+;; 			    (cons (make-label-entry next-inst
+;; 						    insts)
+;; 				  labels)))
+;;                (receive (cons (make-instruction next-inst)
+;;                               insts)
+;;                         labels)))))))
+
+;; (define test-foo
+;;   (make-machine
+;;    '(a)
+;;    '()
+;;    '(start
+;;        (goto (label here))
+;;      here
+;;        (assign a (const 3))
+;;        (goto (label there))
+;;      here
+;;        (assign a (const 4))
+;;        (goto (label there))
+;;      there)))
+
+;; (start test-foo)
+;; (get-register-contents test-foo 'a) ; Error: Label is multiply defined -- here
+
+; Exercise 5.9 -------------------------------------------------------------------
+
+(define (make-operation-exp exp machine labels operations)
+  (let ((op (lookup-prim (operation-exp-op exp) operations))
+        (aprocs
+         (map (lambda (e)
+		(case (car e)
+		  ((const reg) (make-primitive-exp e machine labels))
+		  (else (error "Invalid operation argument --" e))))
+              (operation-exp-operands exp))))
+    (lambda ()
+      (apply op (map (lambda (p) (p)) aprocs)))))
+
+;; (define test-machine
+;;   (make-machine
+;;    '(output)
+;;    (list (list 'do-stuff (lambda (a b) 1223)))
+;;    '(start
+;;        (assign output (op do-stuff) (const 3) (label end))
+;;      end)))
+
+;; (start test-machine)
+;; (get-register-contents test-machine 'output)
+;Invalid operation argument -- (label end)
+
+; Exercise 5.10 -------------------------------------------------------------------
+
