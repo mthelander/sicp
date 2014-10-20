aba3ed17787c365b5339a3edfa97d261270c7d34
Michael Thelander <mjthelander@gmail.com>
2014-10-19 21:09:23 -0700

    Initial setup for 5.4 exercises

---
+++ chapter5/section_5_4.scm
@@ -0,0 +1,6 @@
+; Section 5.4.1 ------------------------------------------------------------------
+
+(load "~/work/sicp/from_book/ch5-regsim.scm")
+(load "~/work/sicp/from_book/ch5-eceval-support.scm")
+(load "~/work/sicp/from_book/ch5-eceval.scm")
+
