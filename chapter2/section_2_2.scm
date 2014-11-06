055d47f994d49d61e338c6544d9c2d93fc0fc26d
Michael Thelander <mjthelander@gmail.com>
2014-11-06 07:42:12 -0800

    Finished exercise 2.52

--- chapter2/section_2_2.scm
+++ chapter2/section_2_2.scm
@@ -735,10 +735,15 @@
 (define (corner-split painter n)
   (if (= n 0)
       painter
-      (let ((up (up-split painter (- n 1)))
-            (right (right-split painter (- n 1))))
-        (let ((top-left (beside up up))
-              (bottom-right (below right right))
-              (corner (corner-split painter (- n 1))))
+      (let ((top-left (up-split painter (- n 1)))
+	    (bottom-right right (right-split painter (- n 1)))
+	    (corner (corner-split painter (- n 1))))
           (beside (below painter top-left)
-                  (below bottom-right corner))))))
+                  (below bottom-right corner)))))
+
+; c.
+
+(define (square-limit painter n)
+  (let ((combine4 (square-of-four identity flip-horiz
+                                  flip-vert rotate180)))
+    (combine4 (corner-split painter n))))
